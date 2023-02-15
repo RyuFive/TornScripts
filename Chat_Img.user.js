e53a3854c6e53f918bf6ca2ddbddf277d1caab10
RyuFive <zohair6995@gmail.com>
2023-02-15 12:21:00 +0500

    Update Chat_Img.user.js

--- Chat_Img.user.js
+++ Chat_Img.user.js
@@ -4,7 +4,7 @@
 // @match      https://www.torn.com/*
 // @namespace    https://github.com/RyuFive/TornScripts/raw/main/Chat_Img.user.js
 // @updateURL    https://github.com/RyuFive/TornScripts/raw/main/Chat_Img.user.js
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @license MIT
