6d677f556f5da136db57013cf4a47f655c3420e7
RyuFive <zohair6995@gmail.com>
2023-02-15 12:20:46 +0500

    Update Auction Names.user.js

--- Auction Names.user.js
+++ Auction Names.user.js
@@ -4,6 +4,7 @@
 // @match      https://www.torn.com/displaycase.php*
 // @match      https://www.torn.com/amarket.php*
 // @namespace    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.user.js
+// @updateURL    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.js
 // @version      0.7
 // @description  try to take over the world!
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
