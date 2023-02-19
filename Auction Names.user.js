6a11ca2938b6d2cf10347a65fd7feee994c236cb
RyuFive <zohair6995@gmail.com>
2023-02-20 01:06:59 +0500

    Update Auction Names.user.js

--- Auction Names.user.js
+++ Auction Names.user.js
@@ -6,7 +6,7 @@
 // @namespace    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.user.js
 // @downloadURL    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.js
 // @updateURL    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.js
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @license MIT
@@ -29,16 +29,18 @@ function refreshData()
     }
 
     for (let i = 0; i < icons.length; i++) {
-        var title = icons[i].parentElement.parentElement.parentElement.getElementsByClassName("title")[0]
-        var removeThis = title.getElementsByClassName("t-gray-6")[0]
-        if (removeThis) {
-            title.removeChild(removeThis)
+        if (document.URL.includes('amarket')) {
+            var title = icons[i].parentElement.parentElement.parentElement.getElementsByClassName("title")[0]
+            var removeThis = title.getElementsByClassName("t-gray-6")[0]
+            if (removeThis) {
+                title.removeChild(removeThis)
+            }
+            if (title.childElementCount >= 1 + icons[i].parentElement.childElementCount) continue
         }
-        if (title.childElementCount >= 1 + icons[i].parentElement.childElementCount) continue
+        else if (icons[i].lastChild.tagName == 'SPAN') continue
 
         var name = icons[i].title.split('>')[1].split('<')[0]
         var value = icons[i].title.split('%')[0].split('>')[3] + "% "
-        var temp
 
         if (name == 'Irradiate' || name == 'Smash') {
             value = ''
@@ -59,11 +61,23 @@ function refreshData()
         else if (name == 'Eviscerate') {
             value = icons[i].title.split(' extra')[0].split('them ')[1] + " "
         }
+        if (document.URL.includes('amarket')) var bonus = document.createElement('p')
+        else bonus = document.createElement('span')
 
-        var bonus = document.createElement('p')
         bonus.innerHTML = value + name
 
-        title.appendChild(bonus)
+        if (document.URL.includes('amarket')) title.appendChild(bonus)
+        else {
+            if (mode == 'dark') {
+                bonus.setAttribute("style", "background-color: #000000b0;")
+            }
+            else {
+                bonus.setAttribute("style", "background-color: #ffffffb0;")
+            }
+            icons[i].appendChild(bonus)
+            icons[i].setAttribute("style", "float:left;white-space: nowrap;right: 0px;padding-left: 0px;top:-40px")
+        }
+        
     }
 
     setTimeout(refreshData, x*1000);
