018cf3253e5a8caac719c1f267c8d3e84e440566
RyuFive <zohair6995@gmail.com>
2023-04-06 21:03:32 +0500

    Update Auction_Names.user.js

--- Auction_Names.user.js
+++ Auction_Names.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Auction Names
 // @author RyuFive
+// @match      https://www.torn.com/displaycase.php*
 // @match      https://www.torn.com/amarket.php*
 // @namespace    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.user.js
 // @downloadURL    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.js
 // @updateURL    https://github.com/RyuFive/TornScripts/raw/main/Auction Names.js
 // @require      https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
-// @version      1.3
+// @version      1.35
 // @description  try to take over the world!
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @license MIT
@@ -14,7 +15,7 @@
 
 var mode = 'dark' // dark or light
 
-function refreshData()
+function amarket()
 {
     $(".t-gray-6").html("")
 
@@ -26,10 +27,8 @@ function refreshData()
 
     for (var i in row) {
         if (!isIntNumber(i)) continue
-        if (document.URL.includes('amarket')) {
-            var title = $(row[i]).find("span.bonus-attachment-icons")[0].title
-            if (title == '') continue
-        }
+        var title = $(row[i]).find("span.bonus-attachment-icons")[0].title
+        if (title == '') continue
 
         var name = title.split('>')[1].split('<')[0]
         var value = format(title, name)
@@ -41,6 +40,35 @@ function refreshData()
             value = format(title, name)
             $(row[i]).find("p.t-gray-6")[0].innerHTML += "<br>" + value + name
         }
+    }
+}
+
+function displaycase() {
+    var icons = $(".bonus-attachment-icons")
+    console.log("hi")
+
+    if (icons.length === 0) {
+        return
+    }
+
+    for (var i in icons) {
+        if (!isIntNumber(i)) continue
+        var title = icons[i].title
+        if (title == '') continue
+
+        var name = title.split('>')[1].split('<')[0]
+        var value = format(title, name)
+
+        var bonus = document.createElement('span')
+        bonus.innerHTML = value + name
+        if (mode == 'dark') {
+            bonus.setAttribute("style", "background-color: #000000b0;")
+        }
+        else {
+            bonus.setAttribute("style", "background-color: #ffffffb0;")
+        }
+        icons[i].appendChild(bonus)
+        icons[i].setAttribute("style", "float:left;white-space: nowrap;right: 0px;padding-left: 0px;top:-40px")
     }
 }
 
@@ -67,4 +95,5 @@ function format(title, name) {
     return value
 }
 
-waitForKeyElements(".item-cont-wrap ", refreshData)
+waitForKeyElements(".item-cont-wrap ", amarket)
+waitForKeyElements(".display-main-page ", displaycase)
