f61161ee10b2779c5542644ff129f25bc67e72ab
RyuFive <zohair6995@gmail.com>
2023-04-06 14:36:07 +0500

    Update Auction_Names.user.js

--- Auction_Names.user.js
+++ Auction_Names.user.js
@@ -10,8 +10,6 @@
 // @description  try to take over the world!
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @license MIT
-// @grant GM_setValue
-// @grant GM_getValue
 // ==/UserScript==
 
 var mode = 'dark' // dark or light
